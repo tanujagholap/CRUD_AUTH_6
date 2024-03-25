1c77db74c1a5b4a237395eb2e08b75ca10074c5f
demo1 <tanujarbhong@gmail.com>
2024-03-25 17:39:41 +0530

    initial commit

---
+++ pro1/app1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Create(models.Model):
+    f_name = models.CharField(max_length=20)
+    s_name = models.CharField(max_length=20)
+    roll = models.IntegerField()
+    subject = models.CharField(max_length=20)
+
